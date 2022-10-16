c255e98bfa61efb027f67eff92f83cd93f81808e
Sirraide <aeternalmail@gmail.com>
2022-10-16 08:25:59 +0200

    Now compiles on Windows

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
-@echo off
-
 rem Download submodules.
 if not exist libs\wxWidgets\README.md (
-    echo "Initialising submodules..."
+    echo Initialising submodules...
     git submodule update --init --recursive
 )
 
+rem Build the project.
 mkdir out
 cd out
 cmake ..
 cmake --build .
+cd ..
