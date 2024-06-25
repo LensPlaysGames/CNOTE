52d2ab25ea9127f5a0206cb35ea203db6f5c9d0c
Lens_r <lensplaysgames@gmail.com>
2024-06-25 01:01:45 -0700

    make clean

--- build.bat
+++
@@ -1,9 +0,0 @@
-rem Download submodules.
-if not exist libs\wxWidgets\include (
-    echo Initialising submodules...
-    git submodule update --init --recursive
-)
-
-rem Build the project.
-cmake -B out
-cmake --build out
